074e97eb07eb83996ba78147620fef5d976bb13f
Jose luis Perez rabasot <jperez-r@c1r1s5.42madrid.com>
2024-01-15 17:52:18 +0100

    actualizacion_15ene

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: jperez-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:30:59 by jperez-r          #+#    #+#             */
-/*   Updated: 2024/01/11 19:31:35 by jperez-r         ###   ########.fr       */
+/*   Updated: 2024/01/15 17:37:19 by jperez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdarg.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <errno.h>
 # include "./libft/libft.h"
 # include "./libft/gnl/get_next_line.h"
 
